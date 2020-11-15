c2b37cba8417ab2ab022025b25602bab27b5f5df
sleeve <1421147715@qq.com>
2020-11-15 22:40:09 +0800

    4.2 常用控键
    1.imageView
    2.progerrBar
    3.AlterDiag

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.kt
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.uiwidgettest
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.View.OnClickListener
+import android.view.View.*
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * View.OnClickListener:使用接口来注册一个监听器
  */
-class MainActivity : AppCompatActivity() ,View.OnClickListener{
+class MainActivity : AppCompatActivity() , OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,17 +22,50 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
 //            var toString = editText.text.toString()
 //            Toast.makeText(this,toString,Toast.LENGTH_SHORT).show()
 //        }
-    }
+        //点击按钮切换图片
+//        button.setOnClickListener {
+//            imageView.setImageResource(R.drawable.img_1)
+//        }
+        //点击按钮改变进度条状态
+//        button.setOnClickListener {
+//            if (progressBar.visibility== VISIBLE){
+//                progressBar.visibility= GONE
+//            }else{
+//                progressBar.visibility= VISIBLE
+//            }
+//        }
 
-    override fun onClick(v: View?) {
-        when (v?.id){
-            R.id.button -> {
-//                点击button获得edittext中的文字
-                val inputText = editText.text.toString()
-                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
+        //点击按钮进度条+10
+//        button.setOnClickListener {
+//            progressBar.progress+=10
+//        }
+
+        //AlterDialog：当前界面弹出一个对话框，指定在所有界面元素之上
+        //屏蔽其他控件，用来警示作用
+        button.setOnClickListener {
+            //构建对话框
+            AlertDialog.Builder(this).apply {
+                setTitle("重要信息")
+                setCancelable(false)
+                setPositiveButton("OK"){dialog, which ->  }
+                setNegativeButton("Cancel"){dialog, which ->  }
+                show()
             }
         }
     }
 
+    override fun onClick(v: View?) {
+//        Log.d("onclick","我执行类")
+//        when (v?.id){
+//            R.id.button -> {
+//                点击button获得edittext中的文字
+//                val inputText = editText.text.toString()
+//                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
+//            }
+//        }
+    }
+
 
 }
+
+
